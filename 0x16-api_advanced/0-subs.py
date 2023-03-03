5a0ac76018b0b5b0adad4b182072364e94972080
ODINGAH001 <edwin.odinga123@gmail.com>
2023-03-03 07:08:19 -0800

    api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+The 0-subs module
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    queries the Reddit API and returns the number of subscribers
+    for a given subrredit
+    """
+    if (subreddit):
+        req = requests.get('https://www.reddit.com/r/{}/about.json'
+                           .format(subreddit),
+                           headers={'User-agent': 'My User Agent 1.0'},
+                           allow_redirects=False)
+    try:
+        subs = (req.json().get('data').get('subscribers'))
+        return (subs)
+    except Exception:
+        return (0)
